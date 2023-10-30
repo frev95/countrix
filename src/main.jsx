a75c1f03b301e8abf81a6969565b616a22a99290
François <gofrev@gmail.com>
2023-10-30 15:44:13 +0100

    1er commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react' // librairie
+import ReactDOM from 'react-dom/client' // librairie
+import App from './App.jsx' // composant
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
